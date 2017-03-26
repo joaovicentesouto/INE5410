63074a5d17ebd45ed2fbcb958e14da0905c23e72
João Vicente Souto <joaovicentesouto@gmail.com>
2017-03-26 16:33:15 -0300

    Melhorando o código do exercício 2

--- processos_no_unix/exercicio02.c
+++ processos_no_unix/exercicio02.c
@@ -6,11 +6,10 @@ int main(int argc, char * argv[]) {
 
     pid_t pid = getpid();
     for(int i = 0; i < 4; i++) {
-        if (pid != 0) {
+        if (pid != 0)
             pid = fork();
-            if (pid != 0)
-                printf("Processo pai %d criou %d\n", getpid(), pid);
-        }
+        if (pid != 0)
+             printf("Processo pai %d criou %d\n", getpid(), pid);
     }
 
     return 0;
