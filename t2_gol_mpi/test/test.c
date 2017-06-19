cbbf5109bd038fe2846836f2d4d0430764d16fa8
João Vicente Souto <joaovicentesouto@gmail.com>
2017-06-19 08:46:51 -0300

    Teste com broadcast

--- t2_gol_mpi/test/test.c
+++ t2_gol_mpi/test/test.c
@@ -5,7 +5,7 @@
 
 
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
 
 	if (argc < 2)
 		return 1;
@@ -18,35 +18,37 @@ int main(int argc, char const *argv[]) {
 
 	if (rank == 0) {
 		int x = atoi(argv[1]);
-		int vetor[x][x];
+		int *vetor = (int *) malloc(x*x*sizeof(int));
 
-		MPI_Send(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 		for (int i = 0; i < x; ++i) {
 			for (int j = 0; j < x; ++j) {
-				vetor[i][j] = i*j;
+				vetor[i*x + j] = i*j;
 			}
 		}
 
-		MPI_Send(&vetor[0], (x-1)*x, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		MPI_Bcast(vetor, x*x, MPI_INT, 0, MPI_COMM_WORLD);
+		//MPI_Send(vetor + (x/2)*x, (x/2)*x, MPI_INT, 2, 0, MPI_COMM_WORLD);
 		//MPI_Recv(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, NULL);
 
+		free(vetor);
 	} else {
 		int x_slave;
-		MPI_Recv(&x_slave, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL);
+		MPI_Bcast(&x_slave, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		//MPI_Recv(&x_slave, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL);
 		
-		int vetor_s[x_slave-1][x_slave];
-		MPI_Recv(&vetor_s, (x_slave-1)*x_slave, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL);
+		int *vetor_s = (int *) malloc(x_slave*x_slave*sizeof(int));
+		MPI_Bcast(vetor_s, x_slave*x_slave, MPI_INT, 0, MPI_COMM_WORLD);
+		//MPI_Recv(&vetor_s, x_slave*x_slave, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL);
 
-		for (int i = 0; i < x_slave-1; ++i) {
-			printf("[");
-			for (int j = 0; j < x_slave; ++j) {
-				printf(" %d ", vetor_s[i][j]);
-			}
-			printf("]\n");
+		printf("Rank: %d [", rank);
+		for (int j = 0; j < x_slave; ++j) {
+			printf(" %d ", vetor_s[(rank-1)*x_slave + j]);
 		}
+		printf("]\n");
 		//MPI_Send(&x_slave, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-
+		free(vetor_s);
 	}
 
 	printf("%d : chegou na barreira\n", rank);
